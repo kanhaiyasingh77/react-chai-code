f5166cb12fd917884a9a491993a34f5ba8170a34
Kanhaiya Singh <kanhaiya11555@gmail.com>
2024-02-25 19:23:21 +0530

    initial commit4

---
+++ 04bgChanger/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,jsx}',
+            './public/index.html' 
+          ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
