b9c399c787198655532cbfc0d3b310883750e906
Kanhaiya Singh <kanhaiya11555@gmail.com>
2024-02-25 18:17:44 +0530

    Add changes to my first react project

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,14 @@
+import Chai from "./chai.js"
+
+function App() {
+  return (
+    <>
+
+    <h1>chai aur code ||  kanhaiya Singh</h1>
+    <Chai/>
+    </>
+
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/chai.js
@@ -0,0 +1,9 @@
+function Chai(){
+    return(
+        <h3>
+            Welcome to my first basic react code
+        </h3>
+    );
+}
+
+export default Chai
