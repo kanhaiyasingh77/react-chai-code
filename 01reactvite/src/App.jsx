a09b25b4e72d219883515ca53c236e76fc9f2321
Kanhaiya Singh <kanhaiya11555@gmail.com>
2024-02-26 09:42:28 +0530

    my first work on react using vite

---
+++ 01reactvite/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from "./chai"
+const username = "kanha@77"
+function App() {
+
+  return (
+    <>
+    <h1>chai aur code || kanhaiya singh.....</h1>
+    <h1>{username}</h1>
+    <Chai/>
+    </>
+    
+  );
+}
+
+export default App
---
+++ 01reactvite/src/chai.jsx
@@ -0,0 +1,9 @@
+function Chai(){
+    return(
+        <>
+        <h1>WElcome to my function</h1>
+        <h2>Chai is ready!!</h2>
+        </>
+    );
+}
+export default Chai
